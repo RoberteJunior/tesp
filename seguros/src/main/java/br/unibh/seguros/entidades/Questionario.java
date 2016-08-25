73a398a0361505193e39b7b9f084cbf76f8508d9
11424772 <11424772@ESTB7LI0308.unibh.edu>
2016-08-25 20:52:39 -0300

    atualização

---
+++ seguros/src/main/java/br/unibh/seguros/entidades/Questionario.java
@@ -0,0 +1,20 @@
+package br.unibh.seguros.entidades;
+
+import java.util.Date;
+
+public class Questionario {
+	private Long id;
+	private Boolean seguradoEPrincipal;
+	private String nomeCondutor;
+	private Boolean resideComPessoa17A25Anos;
+	private Boolean possuiGaragemResidencia;
+	private Boolean possuiGaragemTrabalho;
+	private Boolean possuiGaragemEstudo;
+	private Boolean residenciaPropria;
+	private Long quilometragemAtual;
+	private Boolean utilizaVeiculoAtividadeProfissionais;
+	private Boolean acidenteOuRouboUltimos2Anos;
+	private Boolean possuiDispositivosAntifurto;
+	private Long version;
+
+}
